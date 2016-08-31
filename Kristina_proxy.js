f9d3d419743cba984e082f22e8bfc9090fdc2055
ludost <ludo@almende.org>
2016-08-31 09:57:33 +0000

    Local changes

--- Kristina_proxy.js
+++ Kristina_proxy.js
@@ -1,5 +1,10 @@
 'use strict';
 
+process.chdir('/Users/Administrator/Desktop/NodeJS');
+
+process.env.Path = process.env.Path + ";C:\\Program Files (x86)\\ffmpeg\\bin";
+console.log(process.env.Path);
+
 console.log("New server");
 var nodeStatic = require('node-static');
 var ffmpeg = require('fluent-ffmpeg');
@@ -29,6 +34,7 @@ var server = http.createServer(options, function(req, res) {
   fileServer.serve(req, res);
 })
 
+//Video:
 var mystream = new require('stream').Transform();
 mystream._transform = function (chunk,encoding,done) 
 {
@@ -42,7 +48,7 @@ mystream.on('error',function (err){
 ;
 
 var command = ffmpeg().input(mystream)
-  .inputOptions('-loglevel verbose')
+  //.inputOptions('-loglevel verbose')
   .inputFormat('webm')
   .noAudio()
   .videoCodec('libx264')
@@ -58,7 +64,7 @@ var command = ffmpeg().input(mystream)
     console.log('Spawned Ffmpeg with command: ' + commandLine);
   })
  .on('progress', function(progress) {
-    console.log('Processing: ',progress);
+    //console.log('Processing: ',progress);
   })
   .on('stderr', function(stderrLine) {
     console.log('Stderr output: ' + stderrLine);
@@ -72,20 +78,64 @@ var command = ffmpeg().input(mystream)
 .run();
 
 
+//Audio:
+var myAudioStream = new require('stream').Transform();
+myAudioStream._transform = function (chunk,encoding,done) 
+{
+    this.push(chunk)
+    done()
+}
+myAudioStream.on('error',function (err){
+    console.log("stream error",err);
+}).on('pause',function(){ console.log("Audio paused")})
+.on('end',function(){ console.log("Audio ended")})
+;
+
+//Spawned Ffmpeg with command: ffmpeg -loglevel verbose -f webm -i pipe:0 -acodec copy -vn -f mpegts -preset veryfast -tune zerolatency udp://127.0.0.1:2222
+var command = ffmpeg().input(myAudioStream)
+  //.inputOptions('-loglevel verbose')
+  .inputFormat('webm')
+  .noVideo()
+  .audioCodec('copy')
+  .format('mpegts')
+  .outputOptions(
+  '-preset','veryfast','-tune','zerolatency')
+//  .output('udp://192.168.173.1:1111')
+  .output('udp://127.0.0.1:2222')
+  .on('start', function(commandLine) {
+    console.log('Spawned Ffmpeg with command: ' + commandLine);
+  })
+ .on('progress', function(progress) {
+    //console.log('Audio Processing: ',progress);
+  })
+  .on('stderr', function(stderrLine) {
+    console.log('Audio Stderr output: ' + stderrLine);
+  }).on('codecData', function(data) {
+    console.log('Input is ' + data.audio + ' audio ' +
+      'with ' + data.video + ' video');
+  })
+  .on('end',function(){
+    console.log('Audio ffmpeg ended');
+  })
+.run();
+
+//Websocket (Both audio and video):
 var wss = new WebSocket({server:server});
 wss.on('connection', function(ws){
   console.log("New connection");
 
   ws.on('message', function(data, flags){
-    console.log("Received data: " + data.length, flags);
+    //console.log("Received data: " + data.length, flags);
     mystream.write(data);
     mystream.resume();
-  });
+    myAudioStream.write(data);
+    myAudioStream.resume();
+	});
 
   ws.on('close', function(){
     console.log("Disconnected");
   });
 
 });
-server.listen(8080);
+server.listen(16160);
 
