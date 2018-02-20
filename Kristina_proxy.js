30ccf2ce6a2b1e052704554ef93a54052874d6a4
Ludo Stellingwerff <ludo@almende.org>
2018-02-20 07:57:23 +0100

    More updates for final version

--- Kristina_proxy.js
+++ Kristina_proxy.js
@@ -24,8 +24,10 @@ fs.access('/Users/Administrator/Desktop/NodeJS', fs.F_OK, function (err) {
 
 
 var options = {
-  key: fs.readFileSync('cert/ec2-52-29-254-9.key'),
-  cert: fs.readFileSync('cert/ec2-52-29-254-9.crt'),
+  key: fs.readFileSync('cert/localhost.key'),
+  cert: fs.readFileSync('cert/localhost.crt'),
+//    key: fs.readFileSync('cert/ec2-52-29-254-9.key'),
+//    cert: fs.readFileSync('cert/ec2-52-29-254-9.crt'),
   agent: false,
   requestCert: true,
   rejectUnauthorized: false
@@ -215,7 +217,7 @@ exportFuncs.cancelRest = function (reservation) {
   if (!reservation_token && reservation.reservation) {
     reservation_token = reservation.reservation.token;
   }
-  if (timeslot.contents.token && reservation_token && (timeslot.contents.token === reservation_token)) {
+  if (timeslot && timeslot.contents && timeslot.contents.token && reservation_token && (timeslot.contents.token === reservation_token)) {
     deleteTimeslot(timeslot);
     storeTimeslot(timeslot.start, new Date().getTime(), timeslot.contents);
   }
@@ -316,10 +318,10 @@ var videoCommand = ffmpeg().input(mystream)
   .noAudio()
   .videoCodec('libx264')
   .format('mpegts')
-  .fps(5)
+  .fps(15)
   .outputOptions(
     '-map', '0', '-preset', 'veryfast', '-tune', 'zerolatency',
-    '-filter:v', 'fps=5', '-x264opts',
+    '-filter:v', 'fps=15', '-x264opts',
     'crf=20:vbv-bufsize=100:vbv-maxrate=3000:intra-refresh=1:slice-max-size=1500:keyint=1:scenecut=-1:ref=1')
   //  .output('udp://192.168.173.1:1111')
   .output('udp://127.0.0.1:1111')
